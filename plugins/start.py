e8a2461daccc1721dd80c672011edc2eb7ae4af6
MINDHELL <Tgpurpose640@gmail.com>
2025-01-14 13:32:47 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -74,7 +74,7 @@ async def start_command(client: Client, message: Message):
 
             if AUTO_DELETE_TIME and AUTO_DELETE_TIME > 0:
 
-                try:
+               try:
                     copied_msg_for_deletion = await msg.copy(chat_id=message.from_user.id, caption=caption, parse_mode=ParseMode.HTML, reply_markup=reply_markup, protect_content=PROTECT_CONTENT)
                     if copied_msg_for_deletion:
                         track_msgs.append(copied_msg_for_deletion)
