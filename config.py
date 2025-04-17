69a61677d9093fb62e2aecada98657a03e821806
MINDHELL <Tgpurpose640@gmail.com>
2025-04-17 11:30:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,26 +8,26 @@ from logging.handlers import RotatingFileHandler
 load_dotenv()
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8176934534:AAHOXsiVYUchZPl73kPNuTnm7sMg2F0pTSI")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7806730990:AAGcLRszllY3xoHIAYMhHB8sZWzLgJZnlWI")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "27788368"))
+APP_ID = int(os.environ.get("APP_ID", "27083483"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "9df7e9ef3d7e4145270045e5e43e1081")
+API_HASH = os.environ.get("API_HASH", "1ba790464745c13ce149649d73137e52")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002295794847"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002690400376"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "7568970591"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7246766108"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://aarshhub:6L1PAPikOnAIHIRA@cluster0.6shiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "AdultDatabase")
+DB_NAME = os.environ.get("DATABASE_NAME", "javbase")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
@@ -36,11 +36,11 @@ JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", None)
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_PIC = os.environ.get("START_PIC","https://envs.sh/vD5.jpg")
+START_PIC = os.environ.get("START_PIC","https://envs.sh/3JI.jpg")
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\n𝘐 𝘈𝘔 𝘊𝘖𝘕𝘛𝘌𝘕𝘛 𝘗𝘙𝘖𝘝𝘐𝘋𝘌𝘙 𝘉𝘖𝘛 😌 .\n𝘐 𝘞𝘐𝘓𝘓 𝘗𝘙𝘖𝘝𝘐𝘋𝘌  𝘉𝘌𝘚𝘛 𝘖𝘍 𝘊𝘖𝘕𝘛𝘌𝘕𝘛 𝘍𝘖𝘙 𝘠𝘖𝘜 ⚡️ .")
 try:
     ADMINS=[6860316927]
-    for x in (os.environ.get("ADMINS", "6860316927").split()):
+    for x in (os.environ.get("ADMINS", "6860316927 7246766108").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -52,11 +52,11 @@ FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
 #set True if you want to prevent users from forwarding files from bot
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
 # Auto delete time in seconds.
-AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "43200"))
-AUTO_DELETE_MSG = os.environ.get("AUTO_DELETE_MSG", "𝐓𝐡𝐚𝐧𝐤𝐬 𝐟𝐨𝐫 𝐮𝐬𝐢𝐧𝐠 😊")
+AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "600"))
+AUTO_DELETE_MSG = os.environ.get("AUTO_DELETE_MSG", "𝐓𝐡𝐚𝐧𝐤𝐬 𝐟𝐨𝐫 𝐮𝐬𝐢𝐧𝐠 😊,\n This file will Be deleted in 10 minutes ,please forward to your saved files.")
 AUTO_DEL_SUCCESS_MSG = os.environ.get("AUTO_DEL_SUCCESS_MSG", "𝐓𝐡𝐚𝐧𝐤𝐬 𝐟𝐨𝐫 𝐮𝐬𝐢𝐧𝐠 😊")
 
 #Set true if you want Disable your Channel Posts Share button
@@ -66,7 +66,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages. join @allvidsbackup1 !"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(6860316927)
+ADMINS.append(7246766108)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
