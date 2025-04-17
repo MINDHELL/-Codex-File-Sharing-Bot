f10bf98f977751939ddb704af9148511e17e447c
MINDHELL <Tgpurpose640@gmail.com>
2025-04-17 11:50:11 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='https://t.me/Xsupprt3bot'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/Xsupprt3bot'>Click here</a>\n○ Channel : @javpostr \n○ Support Group : @Xsupport_chats</b>",
+            text = f"<b>○ Creator : <a href='https://t.me/Xsupprt3bot'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/Xsupprt3bot'>Click here</a>\n○ Channel : @javpostr \n○ Support Group : @javposts</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
